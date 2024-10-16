b78b6d61a67f5e00bf8bc5cad0e34f1fa2320cf8
Anas <anasothman581@gmail.com>
2024-10-16 20:05:25 +0300

    api

--- app/__init__.py
+++ app/__init__.py
@@ -114,3 +114,99 @@ def create_app():
       app.logger.info('Application startup')
 
     return app
+
+
+"""
+the api
+!User
+post {{url}}/register
+{
+    "username":"anas2",
+    "email":"anasothman234@gmail.com",
+    "password1":"Flaskpassword@24"
+}
+
+post {{url}}/login
+{
+    "email":"anasothman@gmail.com",
+    "password1":"Flaskpassword@24"
+}
+
+get {{url}}/user-info
+
+
+post {{url}}/logout
+
+!Topics
+
+get {{url}}/topics
+
+post {{url}}/topics
+{
+    "name": "Controller",
+    "category_id":"f39ea3ce-5abd-47a8-965f-04d915c1e555"
+}
+
+get {{url}}/topics/799247f5-af34-457d-a2ec-c9dfca8585e3
+
+delete {{url}}/topics/96e025ac-d10c-495f-9893-5472280f4786
+
+put {{url}}/topics/799247f5-af34-457d-a2ec-c9dfca8585e3
+
+!Series
+
+get {{url}}/brand/series
+
+get {{url}}/brand/285ca083-678b-44ae-afb8-dea85939f23a/series
+
+post {{url}}/brand/285ca083-678b-44ae-afb8-dea85939f23a/series
+
+get {{url}}/brand/d198f699-d6c5-4356-9cf6-793c75f0fc8a/series/a6bd590c-aad7-4fb0-97e6-daac1f7e725b
+
+delete {{url}}/brand/285ca083-678b-44ae-afb8-dea85939f23a/series/9cb2f956-dd34-46ba-8f2f-7f32f4402bc9
+
+put {{url}}/brand/285ca083-678b-44ae-afb8-dea85939f23a/series/a6bd590c-aad7-4fb0-97e6-daac1f7e725b
+
+!Product
+
+post {{url}}/product
+
+get {{url}}/product
+
+get {{url}}/product/06b5b33c-da27-4c95-9080-ede4beec4931
+
+delete {{url}}/product/06b5b33c-da27-4c95-9080-ede4beec4931
+
+!Categories
+
+get {{url}}/category
+
+post {{url}}/category
+
+get {{url}}/category/c9ec1444-0062-44cf-bc8d-559403731f62
+
+delete {{url}}/category/04bc6898-71ed-449e-88e8-2697419fdebf
+
+put {{url}}/category/46b908ec-10b8-4f3a-9c1c-8dfb5f13a887
+
+!Brand
+
+get {{url}}/brand
+
+post {{url}}/brand
+
+get {{url}}/brand/285ca083-678b-44ae-afb8-dea85939f23a
+
+delete {{url}}/brand/6c71dd49-9fec-4425-b81b-2376abfb4c21
+
+put {{url}}/brand/05b56ddf-3567-4023-a44d-f79a98650818
+
+!Admin
+
+get {{url}}/admin/user-info
+
+get {{url}}/admin/user-info/723cb356-69af-4875-982a-d08ee4b547b4
+
+delete {{url}}/admin/user-info/4722de00-e3ff-4d77-945a-30d6cd743ba8
+
+"""
